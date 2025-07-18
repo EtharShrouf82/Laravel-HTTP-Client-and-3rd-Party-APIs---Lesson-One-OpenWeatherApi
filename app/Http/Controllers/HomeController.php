786a350d5a1854bcc6776087787d6afa93bf4daf
Ethar Nidal Shrouf <etharshrouf@Ethars-MacBook-Pro.local>
2025-07-18 04:13:34 +0300

    first commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\WeatherServiceInterface;
+use App\Services\WeatherService;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(WeatherServiceInterface $weatherService)
+    public function index(WeatherService $weatherService)
     {
         $weatherData = $weatherService->getCurrentWeather();
         return view('welcome', compact('weatherData'));
